6d22479a2b0f92742a23849bf500563cb063a4e9
Omolabake <oladejiomolabake14@gmail.com>
2023-04-20 15:41:10 +0100

    Changed Book Details from tabular to word format

--- app2/Controllers/BookrecsController.cs
+++ app2/Controllers/BookrecsController.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
 using System.Linq;
 using System.Net;
-using System.Web;
 using System.Web.Mvc;
-using app2.IServices;
 using app2.Models;
 using app2.Services;
 
@@ -16,6 +12,7 @@ namespace app2.Controllers
     {
         private readonly BookRecordService _bookRecordService;
 
+        //Constructor
         public BookrecsController()
         {
             _bookRecordService = new BookRecordService();
